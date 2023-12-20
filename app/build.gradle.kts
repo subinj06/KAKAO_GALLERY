bde1eadd2929993de10e1b563b4599ea50455f59
subinj06 <60subin@naver.com>
2023-12-20 20:20:49 +0900

    remove floating

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.kakao_gallery"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kakao_gallery"
